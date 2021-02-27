7bd63e7226cb445a00149c828a65535a00499790
Domizio Demichelis <dd.nexus@gmail.com>
2021-02-27 19:08:37 +0700

    indentation and syntax normalization; small fixes

--- fm.zsh
+++ fm.zsh
@@ -12,50 +12,50 @@ autoload -Uz $fm__root/autoload/**/*(.)
 
 fm__command_keys=(
 # FM UI
-    ${FM__KEY_MENU:=ctrl-space}
-    ${FM__KEY_PREVIEW:=ctrl-p}
-    #FM__KEY_SELECT_ALL bind key
-    ${FM__KEY_REFRESH=ctrl-r}
-    ${FM__KEY_NOTIFY_WD=ctrl-w}
-    ${FM__KEY_HIDDEN=ctrl-h}
-    ${FM__KEY_SORT=ctrl-s}
-    ${FM__KEY_QUIT=ctrl-q}
+	${FM__KEY_MENU:=ctrl-space}
+	${FM__KEY_PREVIEW:=ctrl-p}
+	#FM__KEY_SELECT_ALL bind key
+	${FM__KEY_REFRESH=ctrl-r}
+	${FM__KEY_NOTIFY_WD=ctrl-w}
+	${FM__KEY_HIDDEN=ctrl-h}
+	${FM__KEY_SORT=ctrl-s}
+	${FM__KEY_QUIT=ctrl-q}
 #FM Views
-    ${FM__KEY_VIEW_ITEM:=enter}
-    ${FM__KEY_VIEW_DIR=alt-d}
-    ${FM__KEY_VIEW_FIND=alt-f}
-    ${FM__KEY_VIEW_GREP=alt-g}
-    ${FM__KEY_VIEW_REC=alt-r}
-    ${FM__KEY_VIEW_FAV=alt-v}
+	${FM__KEY_VIEW_ITEM:=enter}
+	${FM__KEY_VIEW_DIR=alt-d}
+	${FM__KEY_VIEW_FIND=alt-f}
+	${FM__KEY_VIEW_GREP=alt-g}
+	${FM__KEY_VIEW_REC=alt-r}
+	${FM__KEY_VIEW_FAV=alt-v}
 # Navigation
-    ${FM__KEY_CD_HOME=alt-h}
-    ${FM__KEY_CD_ROOT=alt-/}
-    ${FM__KEY_CD_PARENT=alt-p}
-    ${FM__KEY_CD_LAST=alt-l}
-    ${FM__KEY_CD_START=alt-s}
-    ${FM__KEY_CD_CONTAIN=alt-c}
+	${FM__KEY_CD_HOME=alt-h}
+	${FM__KEY_CD_ROOT=alt-/}
+	${FM__KEY_CD_PARENT=alt-p}
+	${FM__KEY_CD_LAST=alt-l}
+	${FM__KEY_CD_START=alt-s}
+	${FM__KEY_CD_CONTAIN=alt-c}
 # FS Commands
-    ${FM__KEY_FAV_ADD_WD=ctrl-alt-w}
-    ${FM__KEY_FAV_ADD=ctrl-alt-s}
-    ${FM__KEY_MARK=ctrl-alt-m}
-    ${FM__KEY_LINK=ctrl-alt-l}
-    ${FM__KEY_NEW_FILE=ctrl-alt-f}
-    ${FM__KEY_NEW_DIR=ctrl-alt-d}
-    ${FM__KEY_COPY=ctrl-alt-c}
-    ${FM__KEY_CUT=ctrl-alt-x}
-    ${FM__KEY_PASTE=ctrl-alt-v}
-    ${FM__KEY_RENAME=ctrl-alt-n}
-    ${FM__KEY_REMOVE=ctrl-alt-r}
-    ${FM__KEY_EXEC_ON=''}
-    ${FM__KEY_EXEC_OFF=''}
+	${FM__KEY_FAV_ADD_WD=ctrl-alt-w}
+	${FM__KEY_FAV_ADD=ctrl-alt-s}
+	${FM__KEY_MARK=ctrl-alt-m}
+	${FM__KEY_LINK=ctrl-alt-l}
+	${FM__KEY_NEW_FILE=ctrl-alt-f}
+	${FM__KEY_NEW_DIR=ctrl-alt-d}
+	${FM__KEY_COPY=ctrl-alt-c}
+	${FM__KEY_CUT=ctrl-alt-x}
+	${FM__KEY_PASTE=ctrl-alt-v}
+	${FM__KEY_RENAME=ctrl-alt-n}
+	${FM__KEY_REMOVE=ctrl-alt-r}
+	${FM__KEY_EXEC_ON=''}
+	${FM__KEY_EXEC_OFF=''}
 # Tools
-    ${FM__KEY_OPEN=ctrl-o}
-    ${FM__KEY_EDIT=ctrl-e}
-    ${FM__KEY_NEW_SHELL=ctrl-n}
-    ${FM__KEY_COPY_PATH=ctrl-t}
+	${FM__KEY_OPEN=ctrl-o}
+	${FM__KEY_EDIT=ctrl-e}
+	${FM__KEY_NEW_SHELL=ctrl-n}
+	${FM__KEY_COPY_PATH=ctrl-t}
 # extra keys
-    double-click       # default fzf alternative of accept
-    esc ctrl-c ctrl-z  # exit/interrupt/suspend
+	double-click       # default fzf alternative of accept
+	esc ctrl-c ctrl-z  # exit/interrupt/suspend
 )
 : ${FM__CMD_CAT:=$( fm__get-cmd 'bat -f --style=numbers --wrap=never' 'cat -n' )}
 : ${FM__CMD_LS:=$( fm__get-cmd 'exa -lbg --color=always --color-scale --group-directories-first' 'ls -lh --color --group-directories-first' )}
@@ -85,44 +85,48 @@ if [[ ! -d ${FM__TMP:=/tmp/fm} ]] mkdir -p $FM__TMP &>/dev/null
 
 local names=( boot dev etc home opt proc sys tmp usr var )
 for line in "${(@f)$($=FM__CMD_LS --color=never /)}"; do
-    if [[ $line == *-\>* ]] continue
-    if [[ $names[(r)${line##* }] ]]; then
-        fm__ls_fields=${=:-$(repeat $((${(w)#line} - 2)) print _) rest}
-        break
-    fi
+	if [[ $line == *-\>* ]] continue
+	if [[ $names[(r)${line##* }] ]]; then
+		fm__ls_fields=${=:-$(repeat $((${(w)#line} - 2)) print _) rest}
+		break
+	fi
 done
 
-typeset -gx fm__root \
-            fm__ls_fields \
-            FM__DATA \
-            FM__TMP \
-            FM__CMD_CAT \
-            FM__CMD_LS \
-            FM__CMD_RIPGREP \
-            FM__CMD_STAT \
-            FM__INFO_COLOR
+typeset -gx \
+		fm__root \
+		fm__ls_fields \
+		FM__DATA \
+		FM__TMP \
+		FM__CMD_CAT \
+		FM__CMD_LS \
+		FM__CMD_RIPGREP \
+		FM__CMD_STAT \
+		FM__INFO_COLOR
 
 local file
-for file in favorites \
-            recent_dirs \
-            preview
-    [[ -f $file ]] || touch $FM__DATA/$file
+for file in \
+		favorites \
+		recent_dirs \
+		preview
+	[[ -f $file ]] || touch $FM__DATA/$file
 
 if [[ ! -s $FM__DATA/preview ]]; then
-    local -A preview=( [command-panel]=1 [command-menu]=1 [widget-panel]=1 [widget-menu]=1 )
-    fm__write -data preview
+	local -A preview=( [command-panel]=1 [command-menu]=1 [widget-panel]=1 [widget-menu]=1 )
+	fm__write -data preview
 fi
 
-for file in added_menu \
-            clipboard \
-            matches \
-            targets
-    [[ -f $file ]] || touch $FM__TMP/$file
+for file in \
+		added_menu \
+		clipboard \
+		matches \
+		targets
+	[[ -f $file ]] || touch $FM__TMP/$file
 : > $FM__TMP/added_menu
 
-for file in $fm__root/preview/panel \
-            $fm__root/preview/menu
-    [[ -x $file ]] || chmod +x $file
+for file in \
+		$fm__root/preview/panel \
+		$fm__root/preview/menu
+	[[ -x $file ]] || chmod +x $file
 
 # load favorites
 hash -df
@@ -133,33 +137,33 @@ source $FM__DATA/favorites
 
 fm__widget_keys=(
 # FM UI
-    $FM__KEY_MENU
-    $FM__KEY_PREVIEW
-    $FM__KEY_REFRESH
-    $FM__KEY_NOTIFY_WD
-    $FM__KEY_HIDDEN
-    $FM__KEY_SORT
-    $FM__KEY_QUIT
+	$FM__KEY_MENU
+	$FM__KEY_PREVIEW
+	$FM__KEY_REFRESH
+	$FM__KEY_NOTIFY_WD
+	$FM__KEY_HIDDEN
+	$FM__KEY_SORT
+	$FM__KEY_QUIT
 #FM Views
-    $FM__KEY_VIEW_ITEM
-    $FM__KEY_VIEW_DIR
-    $FM__KEY_VIEW_FIND
-    $FM__KEY_VIEW_GREP
-    $FM__KEY_VIEW_REC
-    $FM__KEY_VIEW_FAV
+	$FM__KEY_VIEW_ITEM
+	$FM__KEY_VIEW_DIR
+	$FM__KEY_VIEW_FIND
+	$FM__KEY_VIEW_GREP
+	$FM__KEY_VIEW_REC
+	$FM__KEY_VIEW_FAV
 # Navigation
-    $FM__KEY_CD_HOME
-    $FM__KEY_CD_ROOT
-    $FM__KEY_CD_PARENT
-    $FM__KEY_CD_LAST
-    $FM__KEY_CD_START
-    $FM__KEY_CD_CONTAIN
+	$FM__KEY_CD_HOME
+	$FM__KEY_CD_ROOT
+	$FM__KEY_CD_PARENT
+	$FM__KEY_CD_LAST
+	$FM__KEY_CD_START
+	$FM__KEY_CD_CONTAIN
 # widget
-    ${FM__KEY_PICK=ctrl-k}     # widget only
-    ${FM__KEY_PICK_WD=ctrl-d}  # widget only
+	${FM__KEY_PICK=ctrl-k}     # widget only
+	${FM__KEY_PICK_WD=ctrl-d}  # widget only
 # extra keys
-    double-click       # default fzf alternative of accept
-    esc ctrl-c ctrl-z  # exit/interrupt/suspend
+	double-click       # default fzf alternative of accept
+	esc ctrl-c ctrl-z  # exit/interrupt/suspend
 )
 
 : ${FM__WIDGET_FZF_OPTS:="--exact --no-sort --reverse --height=40% --info=inline --color=info:$FM__INFO_COLOR"}
--- launcher/fm.fish
+++ launcher/fm.fish
@@ -1,7 +1,7 @@
 # fm launcher function (fish syntax)
 
 function fm
-    test "$argv[1]" = '-' && cd $fm__start_dir && return 0
-    set fm__start_dir $PWD
-    cd (zsh -i -c "fm $argv >/dev/tty; print -- \$PWD")
+	test "$argv[1]" = '-' && cd $fm__start_dir && return 0
+	set fm__start_dir $PWD
+	cd (zsh -i -c "fm $argv >/dev/tty; print -- \$PWD")
 end
